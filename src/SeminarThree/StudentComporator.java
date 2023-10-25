d83588d6026370a498db6332b96139090218b508
Your Name <you@example.com>
2023-10-25 21:10:44 +0200

    lesson 3

---
+++ src/SeminarThree/StudentComporator.java
@@ -0,0 +1,21 @@
+package SeminarThree;
+
+import java.util.Comparator;
+
+public class StudentComporator  implements Comparator<Student>  {
+    public int compare(Student firstStudent, Student secondStudent) {
+        return firstStudent.getName().compareTo(secondStudent.getName());
+    }
+    public static class StudentAgeComparator implements Comparator<Student> {
+
+        public int compare(Student ageFirstStudent, Student ageSecondStudent) {
+
+            if (ageFirstStudent.getAge() > ageSecondStudent.getAge())
+                return 1;
+            else if (ageFirstStudent.getAge() < ageSecondStudent.getAge())
+                return -1;
+            else
+                return 0;
+        }
+    }
+}
